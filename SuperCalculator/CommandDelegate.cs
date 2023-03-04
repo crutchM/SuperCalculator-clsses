724f7df2c00dd90a76b411469e9ce5de6fd822cb
crutchM <sads1100@gmail.com>
2023-03-04 18:18:32 +0500

    Initial commit

---
+++ SuperCalculator/CommandDelegate.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Windows.Input;
+
+namespace SuperCalculator;
+
+public class CommandDelegate : ICommand
+{
+    private Action<object?> execute;
+    private Func<object?, bool>? canExecute;
+
+    public CommandDelegate(Action<object?> execute, Func<object?, bool>? canExecute = null)
+    {
+        this.execute = execute;
+        this.canExecute = canExecute;
+    }
+
+    public bool CanExecute(object? parameter)
+    {
+        return canExecute == null || canExecute(parameter);
+    }
+
+    public void Execute(object? parameter)
+    {
+        execute(parameter);
+    }
+
+    public event EventHandler? CanExecuteChanged;
+}
---
+++ SuperCalculator/MainViewModel.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace SuperCalculator;
+
+public class MainViewModel : ViewModel
+{
+    private string _value;
+    private double operand;
+    private string Operator;
+
+    public String Value
+    {
+        get => _value;
+        set => SetField(ref _value, value);
+    }
+
+    public ICommand OnDigitClicked => new CommandDelegate(parameter =>
+    {
+        Value += parameter.ToString();
+    });
+    
+    public ICommand OnActionClicked => new CommandDelegate(parameter =>
+    {
+        operand = Double.Parse(Value);
+        Operator = parameter.ToString();
+        Value = "";
+    });
+    
+    public ICommand OnEqualClicked => new CommandDelegate(parameter =>
+    {
+        double secondOperand = Double.Parse(Value);
+        double result = 0;
+        switch (Operator)
+        {
+            case "+":
+                result = operand + secondOperand;
+                break;
+            case "/":
+                result = operand / secondOperand;
+                break;
+            case "*":
+                result = operand * secondOperand;
+                break;
+            case "-":
+                result = operand - secondOperand;
+                break;
+        }
+        Value = $"{result}";
+    });
+}
